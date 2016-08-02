bfdc2ffae45d5bba75ec06055eb791faeb4960a5
colpu <ycg520520@qq.com>
2016-08-02 17:36:21 +0800

    首页修改增加超链接及样式调整

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -573,6 +573,8 @@ tabs.each(function(){
 	_this
 		.on('mouseover','li', function(e) {
 			modifyFn({modifyObj:modify, curMenu:$(this), offset:tabsOffset});
+			var index= $(this).index();
+			_this.parents('.tabs').find('.bd .tabs-item').eq(index).show().siblings('.tabs-item').hide();
 		})
 		.on('mouseleave','li',function(e){
 			modifyFn({modifyObj:modify, curMenu:_this.find('li.active'),offset:tabsOffset});
@@ -587,13 +589,35 @@ if(G.ie()<9){
 			type: 1,
 			title: false,
 			area: ['640px','auto'],
-			closeBtn: true,
+			closeBtn: false,
 			// shade: [0.5, '#000'],
 			shadeClose: false,
 			skin: 'layui-layer-nobg',
 			content: $('#J_browser')
 	});
 }
-$('body').on('click','.J_close',function(){
-	layer.closeAll();
+layer.open({
+		type: 1,
+		title: false,
+		area: ['320px','auto'],
+		closeBtn: false,
+		// shade: [0.5, '#000'],
+		shadeClose: false,
+		skin: 'layui-layer-nobg',
+		content: $('#J_sigin')
+});
+$('body')
+	.on('click','.J_close',function(){
+		layer.closeAll();
+	})
+	.on('click',function(){
+		$('#J_alike').hide()
+	})
+
+$('.J_close').on('click',function(){
+	$('#J_alike').hide();
 })
+$('#search').on('keyup',function(){
+	$('#J_alike').show()
+})
+
--- src/static/js/filter.js
+++ src/static/js/filter.js
@@ -18,3 +18,68 @@ new Swiper('.scroller', {
   freeMode: true,
   resistanceRatio:0
 });
+
+$('body')
+  .on('click','.condition a', function(e){
+    e.stopPropagation();
+    return false;
+  })
+  .on('click', '#J_clearFilter', function(){
+    $('.condition').html('')
+  })
+  .on('click','.ift-close', function(e){
+    e.stopPropagation();
+    var _this = $(this);
+    _this.parents('a').remove();
+    return false;
+  })
+  // 自定义价格操作
+  .on('click','#submit_price', function(){
+    var minPrice = $.trim($('#min_price').val()),
+        maxPrice = $.trim($('#max_price').val()),
+        priceFilter = $('.price-interval').siblings('.attr-val-list').find('a')
+        reg = /^[1-9]\d*\.?\d*$|^0\.\d*$/;
+    if(!reg.test(minPrice) || !reg.test(maxPrice)){
+      console.log('输入错误，必须是有效的数字。')
+      return;
+    }
+    console.log(minPrice, maxPrice)
+    if(Number(minPrice)>Number(maxPrice)){
+      console.log('起始区间数必须大于结束区间数。')
+      return;
+    }
+    priceFilter.removeClass('active');
+    
+  });
+
+// 条件筛选操作
+$('.attr-val-list a').on('click',function(e){
+  e.stopPropagation();
+  var _this = $(this),
+      text = _this.text(),
+      typeText = _this.parents('.attr-row').find('.attr-key').text(),
+      reg = new RegExp(typeText),
+      conditionList = $('.condition a');  
+  
+  _this.addClass('active').siblings('a').removeClass('active');
+
+  // 判断筛选条件区域是否存在，存在就做替换操作，不存在就追加
+  if(reg.test(conditionList.find('span').text())){
+    conditionList.each(function(){
+      var _curThis = $(this);
+      if(_curThis.find('span').text() === typeText){
+        if(text !== "全部")
+          _curThis.find('strong').html(text);
+        else
+          _curThis.remove();
+      }
+    })
+  }else{
+    $('.condition').append('<a href="javascript:;"><span>'+typeText+'</span><strong>'+text+'</strong><i class="ift-close"></i></a>')
+  }
+
+  return false;
+})
+
+
+
